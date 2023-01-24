4dd52f1a3c451a78c5e153b492d5e0d1ebff38c0
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-24 22:00:09 +0000

    version 1 of complex state

---
+++ complex_state/form_version1.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [heading, setHeading] = useState("");
+
+  function handleChange1(e) {
+    setFname(e.target.value);
+  }
+
+  function handleChange2(e) {
+    setLname(e.target.value);
+  }
+
+  function handleClick(e) {
+    setHeading(fname + " " + lname);
+
+    e.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {heading}</h1>
+      // once submitted, the h1 heading should say 'Bob Sleigh' (for example) is 'Bob' typed into first input, and 'Sleigh' in second input
+      <form onSubmit={handleClick}>
+        <input
+          value={fname}
+          onChange={handleChange1}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          value={lname}
+          onChange={handleChange2}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
