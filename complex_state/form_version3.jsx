84fda1f642c8886b5d3e5653ebc0c4c8b1f24489
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-24 23:23:33 +0000

    Create form_version3.jsx

---
+++ complex_state/form_version3.jsx
@@ -0,0 +1,59 @@
+// complex state with three inputs and change state for each input using conditional
+
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(e) {
+    // destructure info first form the synthetic event i.e. name and value of input fields
+    const { name, value } = e.target;
+    
+    // console.log(name)
+    // console.log(value)
+
+    // setFunction 'comes with' a prevValue paramter which can be logged. The 'prevValue' is an object with the defined keys (i.e. fName, lName, email)
+    setContact(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        }
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        }
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        }
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
